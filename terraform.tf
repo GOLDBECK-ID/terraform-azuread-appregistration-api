7b4741fcd44d767e57816b3a22bd6e260ab17bfa
Waldemar Koehler <waldemar.koehler@goldbeck.de>
2023-12-20 10:49:36 +0100

    fmt

--- terraform.tf
+++ terraform.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = "=1.6.4"
   required_providers {
     azuread = {
-      source = "hashicorp/azuread"
+      source  = "hashicorp/azuread"
       version = "~> 2.47.0"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "~> 3.6.0"
     }
   }
