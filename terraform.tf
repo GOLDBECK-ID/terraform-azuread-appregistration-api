8a20d4d971f206e831694c99d11c9bfdcdaa2fb5
Waldemar Köhler <koehlerw@goldbeck.biz>
2024-02-15 06:40:43 +0000

    Reusable modules should constrain only their minimum allowed versions of Terraform and providers, such as >= 0.12.0. This helps avoid known incompatibilities, while allowing the user of the module flexibility to upgrade to newer versions of Terraform without altering the module.

--- terraform.tf
+++ terraform.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 2.47.0"
+      version = ">= 2.47.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.89.0"
+      version = ">= 3.89.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.6.0"
+      version = ">= 3.6.0"
     }
   }
 }
