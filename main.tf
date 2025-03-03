5ac582facd409d3b6554d4bb363dc523677956cc
Waldemar Köhler <koehlerw@goldbeck.biz>
2025-03-03 12:03:53 +0100

    feat: remove single_page_application from lifecycle ignore_changes

--- main.tf
+++ main.tf
@@ -121,7 +121,6 @@ resource "azuread_application" "adappregistration" {
   lifecycle {
     ignore_changes = [
       optional_claims,
-      single_page_application,
       identifier_uris
     ]
   }
