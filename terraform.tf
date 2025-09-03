4f1917dc556e4eef70a1961f1636c6ac6c6fcb76
Waldemar Köhler <koehlerw@goldbeck.biz>
2025-09-03 11:00:54 +0200

    chore(deps): update provider versions in terraform.tf for improved compatibility

--- terraform.tf
+++ terraform.tf
@@ -2,19 +2,19 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = ">= 3.1.0"
+      version = ">= 3.5.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 4.23.0"
+      version = ">= 4.42.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.7.1"
+      version = ">= 3.7.2"
     }
     time = {
       source  = "hashicorp/time"
-      version = ">= 0.13.0"
+      version = ">= 0.13.1"
     }
   }
 }
