5eb03b0afddfe42c76a88239cda054620a6fe28d
Waldemar Köhler <koehlerw@goldbeck.biz>
2025-09-03 11:03:17 +0200

    refactor: simplify azuread_application_pre_authorized resource by using array for permission_ids

--- main.tf
+++ main.tf
@@ -167,17 +167,13 @@ resource "azuread_application_password" "ad_application_password" {
   ]
 }
 
-# resource "random_uuid" "app_reg_pre_authorized" {
-#   count = var.authorized_app_id == null ? 0 : 1
-# }
-
 resource "azuread_application_pre_authorized" "pre_authorized_clients" {
   for_each = var.oauth2_permission_scopes #Besser nicht for_each, sondern ids in array umwandeln
 
   application_id       = azuread_application.adappregistration.id
   authorized_client_id = var.authorized_app_id
   permission_ids = [
-    random_uuid.app_reg_user_impersonation[each.key].result
+    random_uuid.app_reg_user_impersonation[*].result
   ]
 
   depends_on = [random_uuid.app_reg_user_impersonation]
